6c56813b42f0abe3275b1178c43aec674e661008
Jan Kowalski <jan.kowalski@pollub.edu.pl>
2023-10-05 19:21:11 +0200

    dodanie gettera i settera

--- src/main/java/org/example/Konsumer.java
+++ src/main/java/org/example/Konsumer.java
@@ -3,4 +3,20 @@ package org.example;
 public class Konsumer {
     public String name;
     public Integer age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
