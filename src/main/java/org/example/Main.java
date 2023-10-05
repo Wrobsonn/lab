6ce0766ed32fa5f1ad2a613993dd9a9d381eb055
Jan Kowalski <jan.kowalski@pollub.edu.pl>
2023-10-05 18:55:22 +0200

    dodanie czegos nowego

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,6 +13,7 @@ public class Main {
 
             System.out.println("i = " + i);
             System.out.println("Hello");
+            System.out.println("Dodanie czego nowego");
         }
     }
 }
